73bc66efd89b010dd6f3cc6464d67298183dbc74
Payaj <payaj.4@gmail.com>
2018-01-18 17:40:23 -0500

    added comments

--- zillow-scraper-20180116.py
+++ zillow-scraper-20180116.py
@@ -21,71 +21,13 @@ proxies = {'http': 'http://topagenets:Topagents1@us-wa.proxymesh.com:31280', 'ht
 headers = {'User-agent': 'Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3'}
 
 
-
-"""################################################################# 1 Get the region ID ####################################################################
-getregionid = requests.get('http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz1994anr3rij_6ikyi&state=NY&city=10460', headers=headers).content
-soup = BeautifulSoup(getregionid, 'lxml')
-
-regions = soup.findAll('list')
-regions = regions[0]
-
-regionid = []
-for region in regions.findAll('id'):
-    regionid.append(region.text)
-
-regionname = []
-for region in regions.findAll('name'):
-    regionname.append(region.text)
-
-len(regionname)
-
-df = pd.DataFrame(columns=['RegionID', 'Zipcode'], index=range(len(regionname)))
-df['RegionID'] = regionid
-df['Zipcode'] = regionname
-
-zips = pd.read_csv('/Users/payaj/Downloads/NYC_City.csv', dtype='unicode')
-df = pd.merge(df,zips, how='left',on='Zipcode')
-
-with open('/Users/payaj/Downloads/ZillowRegionId-20180106.csv', 'w') as outfile:
-    df.to_csv(outfile, sep=',')#"""
-
-"""################################################################# 2 Get the Agent Url ####################################################################
-df = pd.read_csv('/Users/payaj/Downloads/ZillowRegionId-20180106.csv', dtype='unicode')
-df = df[df['City']=='Bronx']
-df = df.reset_index()
-
-for i in range(1,len(df)):
-    Reference = 0
-    for Page in range(1,26):
-        Agents=requests.get('https://www.zillow.com/new-york-ny-'+df.loc[i,'Zipcode']+'/real-estate-agent-reviews/?page='+str(Page)+'&showAdvancedItems=false&regionID='+df.loc[i,'RegionID']+'&locationText=Bronx%20New%20York%20NY%20'+df.loc[i,'Zipcode'], headers=headers).content
-
-        soup = ''
-        soup = BeautifulSoup(Agents, 'html.parser')
-
-        AgentCards, SellersAgent1, SellersAgent1Url, SellersAgent1Phone = "", "", "", ""
-        AgentCards = soup.findAll('div', {'class':'ldb-contact-summary ldb-fg'})
-        for j in range(len(AgentCards)):
-            SellersAgent1Url= str(AgentCards[j].findAll('a')[0].get('href'))
-            SellersAgent1 = str(AgentCards[j].findAll('a')[0].text)
-            SellersAgent1Phone = str(AgentCards[j].findAll('p', {'class':'ldb-phone-number'})[0].text)
-
-            args = SellersAgent1, SellersAgent1Url, SellersAgent1Phone
-            with open('/Users/payaj/Downloads/zillow-'+df.loc[i,'Zipcode']+'-AgentUrl.csv', 'a') as outfile:
-                writer = csv.writer(outfile)
-                if i == 0 and j == 0:
-                    writer.writerow(
-                        ['SellersAgent1', 'SellersAgent1Url', 'SellersAgent1Phone'])
-                writer.writerow(args)
-
-            if Page != Reference:
-                time.sleep(10 * random.random())
-            Reference = Page
-    print ("region index: "+ str(i))#"""
-
 #"""################################################################# 3 Get the data by Agent ####################################################################
+
+############ df is the csv file with agent profile url (url doesn't contain the domain) #############
 df = pd.read_csv('/Users/payaj/Downloads/zillow-bronx-agents-20180117.csv', dtype='unicode')
 driver = webdriver.Chrome(executable_path = path_to_chromedriver)
 
+######### Below first for loop open the profile of each agent(one by one) in the browser ##########
 for index in range(12,len(df)):
     print df.loc[index,'SellersAgent1Url']
     try:
@@ -93,11 +35,14 @@ for index in range(12,len(df)):
         driver.get('https://www.zillow.com'+df.loc[index,'SellersAgent1Url'])
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         totalPage=''
-
+        
+        ######## below code will find the pagination footer of the "past sales" table (2nd table) ########
         totalPage = soup.findAll('section', {'class': 'sales-history property-listings zsg-content-section'})[0].findAll('ul', {'class': 'pagination zsg-pagination'})[0]
         print 'Stage 1'
         pages = ''
         Reference = 0
+        ########### below for loop will get the last number of the page in the "past sales" table and save it in page in integer format##############################
+        ######################### then will save all the page number in "pages"
         for page in totalPage.findAll('a'):
             #pages.append(str(page.text))
             page = int(str(page.text))
@@ -106,11 +51,14 @@ for index in range(12,len(df)):
         print "Stage 2"
     except:
         print "Nothing on the page"
-
+        
+    ######## below for loop will call all the page numbers inside "pages" one by one ########
     for page in pages:
         time.sleep(2)
         soup = ''
         table = ' '
+        
+        ############## below code will click on the page number of the "past sales" table based on the for loop #############
         try:
             #wait = WebDriverWait(driver, 10)
             #NextButton = wait.until(EC.element_to_be_clickable((By.XPATH, "//section[@class='sales-history property-listings zsg-content-section']//a[text()="+str(page)+"]")))
@@ -119,7 +67,7 @@ for index in range(12,len(df)):
             print "Stage 3"
         except:
             print "No next"
-
+        ############### below section will have the html code for the table #################
         try:
             soup = BeautifulSoup(driver.page_source, 'html.parser')
             time.sleep(2)
@@ -127,7 +75,8 @@ for index in range(12,len(df)):
             print "Stage 4"
         except:
             print "Nothing in table"
-
+        
+        ############## below for loop will iterate through the first page of the table and will write the data in the csv (row by row) ##############
         for i in range(len(table)):
             time.sleep(3)
             Address, City, State, Zipcode, ListingUrl = "","","","",""
@@ -189,15 +138,9 @@ for index in range(12,len(df)):
             if page % 10 == 0:
                 time.sleep(10 * random.random())
             Reference = page
+            ############################### the last for loop for one page of the table (which was writing data in a csv row by row ends here) ##############
         print ("page index: " + str(page))#"""
-
-
-
-
-driver.get('https://www.zillow.com'+df.loc[1,'SellersAgent1Url'])
-'https://www.zillow.com'+df.loc[12,'SellersAgent1Url']
-
-wait = WebDriverWait(driver, 10)
-NextButton = wait.until(EC.element_to_be_clickable((By.XPATH, "//section[@class='sales-history property-listings zsg-content-section']//a[text()=2]")))
-driver.execute_script("arguments[0].click();", NextButton)
-
+        ###################### for loop that was changing the pages of the table ends here #######################
+   
+################### for loop that changes the agent profile from the csv ends here ##########################"""
+        
